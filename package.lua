c1671ef7eeb03d83a30f9d0cfea98818ceab9fb0
Daniel Chu <danielchu096@gmail.com>
2022-09-04 09:20:14 +1200

    Add files via upload

---
+++ package.lua
@@ -0,0 +1,27 @@
+return {
+
+    name = "hashcollision/sweetpie",
+    version = "0.0.0",
+    license = "Unlicense",
+
+    description = " Discord bot written in Luvit + Discordia, generally for music",
+    tags = { "lua", "discord" },
+
+    author = { name = "Gavin", email = "gojinhan2@gmail.com" },
+    homepage = "https://github.com/CozzyBro2/sweetpie",
+
+    dependencies = {
+
+        "SinisterRectus/discordia@2.9.2",
+
+        "creationix/coro-spawn@3.0.2",
+
+    },
+
+    files = {
+
+        "**.lua",
+
+    },
+
+}
